2ff69a01e3a009b672e984f32fe839b23fbd60d5
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-20 20:26:09 -0400

    Revert "Merge branch 'main' of https://github.com/DaisyOfTheDay/ForgsRepo"
    This reverts commit 840d66c62d2178c939c2b6e9e5acb7829255cbf2, reversing
    changes made to 9d07745a6d3b3e9767d6d65a3e8c194888666ab8.

--- Portfolio2Project/Assets/Scripts/Player Select/PlayerSelect.cs
+++ Portfolio2Project/Assets/Scripts/Player Select/PlayerSelect.cs
@@ -8,16 +8,6 @@ using UnityEngine.SceneManagement;
 public class PlayerSelect : MonoBehaviour
 {
     [SerializeField] GameObject player;
-<<<<<<< HEAD:Portfolio2Project/Assets/Scripts/PlayerSelect.cs
-    [SerializeField] NewStaff staff;
-    public void SelectedFire()
-    {
-        Instantiate(player);
-        staff.ChangeElement("Fire");
-        Debug.Log("Selected Fire");
-        Debug.Log("PlayerSpawned");
-        SceneManager.LoadScene("Main Game");
-=======
     [SerializeField] GameObject levelManager;
     [SerializeField] GameObject UI;
     public void SelectedFire()
@@ -25,30 +15,17 @@ public class PlayerSelect : MonoBehaviour
         PrePlayerElementSetup();
         player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Fire;
         PostPlayerElementSetup();
->>>>>>> abceae162203a3ed79a4796f92c902f46baf60ce:Portfolio2Project/Assets/Scripts/Player Select/PlayerSelect.cs
     }
 
     public void SelectedWater()
     {
         PrePlayerElementSetup();
         player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Water;
-<<<<<<< HEAD:Portfolio2Project/Assets/Scripts/PlayerSelect.cs
-        Debug.Log("Selected Water");
-        Debug.Log("PlayerSpawned");
-        SceneManager.LoadScene("Main Game");
-=======
         PostPlayerElementSetup();
->>>>>>> abceae162203a3ed79a4796f92c902f46baf60ce:Portfolio2Project/Assets/Scripts/Player Select/PlayerSelect.cs
     }
 
     public void SelectedEarth()
     {
-<<<<<<< HEAD:Portfolio2Project/Assets/Scripts/PlayerSelect.cs
-        Instantiate(player);
-        staff.ChangeElement("Earth");
-        //player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Earth;
-        Debug.Log("Selected Earth");
-=======
         PrePlayerElementSetup();
         player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Earth;
         PostPlayerElementSetup();
@@ -63,7 +40,6 @@ public class PlayerSelect : MonoBehaviour
 
     public void PostPlayerElementSetup() //must happen after player element setup occurs
     {
->>>>>>> abceae162203a3ed79a4796f92c902f46baf60ce:Portfolio2Project/Assets/Scripts/Player Select/PlayerSelect.cs
         Debug.Log("PlayerSpawned");
         SceneManager.LoadScene("Reception");
         Instantiate(levelManager);
