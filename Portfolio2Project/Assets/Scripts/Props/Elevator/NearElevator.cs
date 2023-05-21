0f392c46aaa095b9720e2d0fb3af909b6e988ad9
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-20 20:26:14 -0400

    Revert "Updated near elevator to put doors back when player walks away"
    This reverts commit 9d07745a6d3b3e9767d6d65a3e8c194888666ab8.

--- Portfolio2Project/Assets/Scripts/Props/Elevator/NearElevator.cs
+++ Portfolio2Project/Assets/Scripts/Props/Elevator/NearElevator.cs
@@ -22,13 +22,4 @@ public class NearElevator : MonoBehaviour
             door2.SetActive(false);
         }
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            door1.SetActive(true);
-            door2.SetActive(true);
-        }
-    }
 }
