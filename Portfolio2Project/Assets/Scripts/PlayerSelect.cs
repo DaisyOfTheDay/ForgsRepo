6e8009901faacc11a28a7868fe96074940a1a354
SeotatopDehsam <jpcoots@student.fullsail.edu>
2023-05-20 20:32:13 -0400

    Please Work

--- Portfolio2Project/Assets/Scripts/PlayerSelect.cs
+++ Portfolio2Project/Assets/Scripts/PlayerSelect.cs
@@ -8,12 +8,10 @@ using UnityEngine.SceneManagement;
 public class PlayerSelect : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    [SerializeField] NewStaff staff;
     public void SelectedFire()
     {
         Instantiate(player);
-        staff.ChangeElement("Fire");
-        Debug.Log("Selected Fire");
+        player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Fire;
         Debug.Log("PlayerSpawned");
         SceneManager.LoadScene("Main Game");
     }
@@ -22,7 +20,6 @@ public class PlayerSelect : MonoBehaviour
     {
         Instantiate(player);
         player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Water;
-        Debug.Log("Selected Water");
         Debug.Log("PlayerSpawned");
         SceneManager.LoadScene("Main Game");
     }
@@ -30,9 +27,7 @@ public class PlayerSelect : MonoBehaviour
     public void SelectedEarth()
     {
         Instantiate(player);
-        staff.ChangeElement("Earth");
-        //player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Earth;
-        Debug.Log("Selected Earth");
+        player.GetComponent<PlayerController>().playerWeaponScript.element = NewStaff.Element.Earth;
         Debug.Log("PlayerSpawned");
         SceneManager.LoadScene("Main Game");
     }
